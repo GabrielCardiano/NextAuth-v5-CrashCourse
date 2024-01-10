6e0e039ff91652f3cd43c4c990838cb30cf9d98b
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-01-10 00:32:00 -0300

    feat: enable error/success validation in server side

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -3,6 +3,7 @@
 import * as z from "zod";
 
 import { useForm } from "react-hook-form";
+import { useState, useTransition } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { LoginSchema } from "@/schemas";
@@ -12,10 +13,15 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "
 import { CardWrapper } from "@/components/auth/card-wrapper";
 import FormError from "@/components/form-error";
 import FormSuccess from "../form-success";
+import { login } from "@/actions/login";
 
 
 
 export function LoginForm() {
+  const [isPending, startTransition] = useTransition();
+  const [error, setError] = useState<string | undefined>('');
+  const [success, setSuccess] = useState<string | undefined>('');
+
   const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
     defaultValues: {
@@ -24,9 +30,20 @@ export function LoginForm() {
     },
   });
 
+  //* This project submit data from client to server using server actions */
+  /*  API route could be another way --> axios.post('/api/route', values); */
   const onSubmit = (values: z.infer<typeof LoginSchema>) => {
-    console.log(values);
-    
+    setError('');
+    setSuccess('');
+
+    startTransition(() => {
+      login(values)
+      .then((data) => {
+        setError(data.error);
+        setSuccess(data.success);
+      })
+
+    });
   }
 
   return (
@@ -53,6 +70,7 @@ export function LoginForm() {
                   <FormControl>
                     <Input
                       {...field}
+                      disabled={isPending}
                       placeholder="john.doe@example.com"
                       type="email"
                     />
@@ -72,6 +90,7 @@ export function LoginForm() {
                   <FormControl>
                     <Input
                       {...field}
+                      disabled={isPending}
                       placeholder="******"
                       type="password"
                     />
@@ -83,13 +102,14 @@ export function LoginForm() {
           </div>
 
           {/* Error/Success login notification */}
-          <FormError message=""/>
-          <FormSuccess message=""/>
+          <FormError message={error} />
+          <FormSuccess message={success} />
 
           {/* Submit button */}
           <Button
             type="submit"
             className="w-full "
+            disabled={isPending}
           >
             Login
           </Button>
