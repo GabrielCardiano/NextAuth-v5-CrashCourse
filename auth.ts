28caeff6656bf05eb6618ab9e98a71508d6414a7
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-01-12 03:03:17 -0300

    feat: config auth callbacks (session, jwt)

--- auth.ts
+++ auth.ts
@@ -4,11 +4,13 @@
  * * File auth-config.ts will trigger the middleware insted auth
 */
 
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import { db } from "./lib/db";
 import authConfig from "@/auth.config";
+import { getUserById } from "@/data/user";
+import { UserRole } from "@prisma/client";
 
 export const {
   handlers: { GET, POST },
@@ -16,7 +18,35 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+
+    async jwt({ token, user }) {
+      if (!token.sub) return token;
+
+      const exististingUser = await getUserById(token.sub);
+      if (!exististingUser) return token;
+
+      token.role = exististingUser.role;
+
+      return token;
+    }
+  },
+
   adapter: PrismaAdapter(db),
-  session: { strategy: "jwt" }, // database strategy when using Prisma does not work on the Edge. Must use jwt.
+
+  session: { strategy: "jwt" }, // database strategy when using Prisma does not work on the Edge. Must use jwt strategy.
+
   ...authConfig,
 })
